20af6cb0d9178c0a2d1787523e8519ac972a1c10
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-01-28 12:07:13 +0530

    Bump version: 0.1.3 → 0.1.4

--- pycdsl/__init__.py
+++ pycdsl/__init__.py
@@ -7,7 +7,7 @@
 
 __author__ = "Hrishikesh Terdalkar"
 __email__ = 'hrishikeshrt@linuxmail.org'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 ###############################################################################
 
