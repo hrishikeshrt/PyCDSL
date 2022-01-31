388739f21cdd056372abf38998dca7231ab7a764
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-02-01 01:36:11 +0530

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hrishikeshrt/pycdsl',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
