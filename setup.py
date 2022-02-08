10af16bf3d6958b5556960b711918fb361d26632
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-02-08 22:11:06 +0530

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hrishikeshrt/PyCDSL',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
