53382078d496470b6ce1ff1e26c15f56fc186c6b
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-02-14 14:21:39 +0530

    "build(release): bump version 0.5.1 → 0.6.0"

--- pycdsl/__init__.py
+++ pycdsl/__init__.py
@@ -11,7 +11,7 @@ Python Interface to Cologne Digital Sanskrit Lexicon (CDSL).
 
 __author__ = "Hrishikesh Terdalkar"
 __email__ = 'hrishikeshrt@linuxmail.org'
-__version__ = '0.5.1'
+__version__ = '0.6.0'
 
 ###############################################################################
 
--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hrishikeshrt/PyCDSL',
-    version='0.5.1',
+    version='0.6.0',
     zip_safe=False,
 )
